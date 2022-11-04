8afaae2b3d3dd83d89875a38a6b6e580355b9572
grononmc <chuaschinai@hotmail.com>
2022-11-03 22:26:34 -0300

    ...

--- src/apps/shared/component/navbar.component.js
+++ src/apps/shared/component/navbar.component.js
@@ -1,16 +1,25 @@
 export default () => {
   return `
-    <nav>
-      <a href="/#" class="nav-link">Home</a>
-      <a href="/#todo" class="nav-link">Todo</a>
-      <a href="/#todoz" class="nav-link">Todoz</a>
-      <a href="/#todozo" class="nav-link">Todozo</a>
+    <nav id="navbar">
+      <label class="toggle-color-mode">
+        <input type="checkbox" name="toggle-color-mode-checkbox" id="toggle-input">
+        <span></span>
+        <i class="fa-solid fa-sun" id="toggle-light"></i>
+        <i class="fa-solid fa-moon" id="toggle-dark"></i>
+      </label>
+
+      <a href="/#" class="nav-link ch-button">
+        <i class="fa-solid fa-house"></i>
+      </a>
+      <a href="/#todo" class="nav-link ch-button">
+        <i class="fa-sharp fa-solid fa-list-ul"></i>
+      </a>
+      <a href="/#todoz" class="nav-link ch-button">
+        <i class="fa-solid fa-gamepad"></i>
+      </a>
+      <a href="/#todozo" class="nav-link ch-button">
+        <i class="fa-solid fa-bomb"></i>
+      </a>
     </nav>
-    <label class="toggle-color-mode">
-      <input type="checkbox" name="toggle-color-mode-checkbox" id="toggle-input">
-      <span></span>
-      <i class="fa-solid fa-sun" id="toggle-light"></i>
-      <i class="fa-solid fa-moon" id="toggle-dark"></i>
-    </label>
   `;
 };
--- src/apps/todo-pomodoro/todo.page.js
+++ src/apps/todo-pomodoro/todo.page.js
@@ -12,26 +12,28 @@ export default () => {
 
   main.innerHTML = `
     ${navbarComponent()}
-    <div id="header-todo">
-      <h1>TODO</h1>
-      <span>v0.1</span>
-      <button id="pomodoro-btn">
-        pomodoro
-        <i class="fa-solid fa-tomato"></i>
-      </button>
-      <span class="js-invisible" id="pomodoro-counter">${window.pomodoro.timer.stringify()}</span>
+    <div id="container-content">
+      <div id="header-todo">
+        <h1>TODO</h1>
+        <span>v0.1</span>
+        <button id="pomodoro-btn">
+          pomodoro
+          <i class="fa-solid fa-clock"></i>
+        </button>
+        <span class="js-invisible" id="pomodoro-counter">${window.pomodoro.timer.stringify()}</span>
+      </div>
+      <div class="container-todo">
+        <input
+          type="text"
+          id="input-task-description"
+          name="input-task-description"
+          placeholder="Enter new task"
+        />
+        ${pomodoroPopupComponent()}
+        <div class="container-task-list" id="container-task-list"></div>
+      </div>
+      <footer>By ChapandoBit</footer>
     </div>
-    <div class="container-todo">
-      <input
-        type="text"
-        id="input-task-description"
-        name="input-task-description"
-        placeholder="Enter new task"
-      />
-      ${pomodoroPopupComponent()}
-      <div class="container-task-list" id="container-task-list"></div>
-    </div>
-    <footer>By ChapandoBit</footer>
   `;
 
   return new ChapandoElement(main, script);
